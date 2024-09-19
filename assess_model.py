f7e243bfc8d5937f4b439d4ac3c75c2a4bf13832
Charlotte-Wurtzen <charlotte.wurtzen@gmail.com>
2024-09-19 15:23:08 +0200

    add comparison of seq len distribution

--- assess_model.py
+++ assess_model.py
@@ -35,23 +35,24 @@ def kl_aa_compare(file1, file2):
 
 
 def seq_len_compare(file1, file2):
-    # Load the distributions
-    df1 = pd.read_csv(file1)
-    df2 = pd.read_csv(file2)
+    # Load the data into pandas DataFrames
+    # Assuming the data is in two separate CSV files: 'file1.csv' and 'file2.csv'
+    data1 = pd.read_csv(file1)
+    data2 = pd.read_csv(file2)
 
-    print(df1['sequence_lengths'])
-    print(df2['sequence_lengths'])
+    # Make sure both distributions have the same support (i.e., same sequence lengths)
+    # Merge the two datasets to align the sequence lengths
+    merged_data = pd.merge(data1, data2, on='sequence_lengths', how='outer').fillna(0)
+    print(merged_data)
 
-    # Ensure the files have the same sequence lengths
-    merged_df = pd.merge(df1[['counts', 'sequence_lengths']],
-                         df2[['counts', 'sequence_lengths']],
-                         on=['sequence_lengths'],
-                         suffixes=('_p', '_q'))
-    print(merged_df)
-    # convert counts to relative frequency?
-    #
+    # Recompute the normalized counts after merging
+    P = merged_data['counts_x'] / merged_data['counts_x'].sum()
+    Q = merged_data['counts_y'] / merged_data['counts_y'].sum()
 
-    return NotImplemented
+    # Compute KL Divergence
+    kl_divergence = entropy(P, Q)
+
+    return kl_divergence
 
 
 def kmer_freq(seqs, kmer):
@@ -59,21 +60,32 @@ def kmer_freq(seqs, kmer):
 
 
 # Compare for AA distribution
-orig_aa_file = 'reports_output/orig/my_instruction/analysis_AA_analysis/report/amino_acid_frequency_distribution.tsv'
-pwm_aa_file = 'reports_output/generated/PWM/my_instruction/analysis_AA_analysis/report/amino_acid_frequency_distribution.tsv'
-vae_aa_file = 'reports_output/generated/VAE/my_instruction/analysis_AA_analysis/report/amino_acid_frequency_distribution.tsv'
+root_path = Path.cwd()
+analysis_path = root_path / 'results/dataset1/analysis'
+aa_path = 'my_instruction/analysis_AA_analysis/report/amino_acid_frequency_distribution.tsv'
+orig_aa_file = root_path / analysis_path / 'orig' / aa_path
+pwm_aa_file = root_path / analysis_path / 'PWM' / aa_path
+vae_aa_file = root_path / analysis_path / 'VAE' / aa_path
 print(f'KL-Divergence for AA distribution (orig vs. PWM): {kl_aa_compare(orig_aa_file, pwm_aa_file)}')
 print(f'KL-Divergence for AA distribution (orig vs. VAE): {kl_aa_compare(orig_aa_file, vae_aa_file)}')
 
-# Check for kmer
-root_path = Path.cwd()
-simulated_seqs_path = root_path / 'data/SLG_dataset_output/my_sim_inst/batch1.tsv'
-PWM_generated_seqs_path = root_path / 'genModel_output/PWM/my_train_gen_model_inst/generated_sequences/batch1.tsv'
-VAE_generated_seqs_path = root_path / 'genModel_output/VAE/my_train_gen_model_inst/generated_sequences/batch1.tsv'
+# Compare for sequence length distribution
+seq_len_path = 'my_instruction/analysis_SeqLen_analysis/report/sequence_length_distribution.csv'
+orig_seq_len_file = root_path / analysis_path / 'orig' / seq_len_path
+pwm_seq_len_file = root_path / analysis_path / 'PWM' / seq_len_path
+vae_seq_len_file = root_path / analysis_path / 'VAE' / seq_len_path
+print(f'KL-Divergence for sequence length distribution (orig vs. PWM): {seq_len_compare(orig_seq_len_file, pwm_seq_len_file)}')
+#print(f'KL-Divergence for sequence length distribution (orig vs. VAE): {seq_len_compare(orig_seq_len_file, vae_seq_len_file)}')
 
-train_seqs = pd.read_csv(simulated_seqs_path, sep='\t')['sequence_aa']
-PWM_gen_seqs = pd.read_csv(PWM_generated_seqs_path, sep='\t')['sequence_aa']
-VAE_gen_seqs = pd.read_csv(VAE_generated_seqs_path, sep='\t')['sequence_aa']
+# Check for kmer
+model_seq_path = 'my_train_gen_model_inst/generated_sequences/batch1.tsv'
+orig_seqs_file = root_path / 'results/dataset1/simulation/my_sim_inst/batch1.tsv'
+PWM_seqs_file = root_path / 'results/dataset1/models/PWM' / model_seq_path
+VAE_seqs_file = root_path / 'results/dataset1/models/VAE' / model_seq_path
+
+train_seqs = pd.read_csv(orig_seqs_file, sep='\t')['sequence_aa']
+PWM_gen_seqs = pd.read_csv(PWM_seqs_file, sep='\t')['sequence_aa']
+VAE_gen_seqs = pd.read_csv(VAE_seqs_file, sep='\t')['sequence_aa']
 print(f'Kmer freq in train data: {kmer_freq(train_seqs, "SLG")}')
 print(f'Kmer freq in generated data (PWM): {kmer_freq(PWM_gen_seqs, "SLG")}')
 print(f'Kmer freq in generated data (VAE): {kmer_freq(VAE_gen_seqs, "SLG")}')
