8ed3955695f1e3f6c6ebc315f14bfce042bc0f19
Charlotte-Wurtzen <charlotte.wurtzen@gmail.com>
2025-02-17 14:42:00 +0100

    small seed fix

--- scripts/sample_sequences.py
+++ scripts/sample_sequences.py
@@ -1,10 +1,7 @@
 import pandas as pd
-import numpy as np
 
 
 def subsample_data(input_file, output_file, sample_size: int = 1000, seed: int = 42):
-
-    np.random.seed(seed)
 
     df = pd.read_csv(input_file)
 
@@ -12,8 +9,16 @@ def subsample_data(input_file, output_file, sample_size: int = 1000, seed: int =
         print(f"Input file has only {len(df)} rows, sampling all rows.")
         sampled_df = df
     else:
-        sampled_df = df.sample(n=sample_size, random_state=42)
+        sampled_df = df.sample(n=sample_size, random_state=seed)
 
     sampled_df.to_csv(output_file, index=False)
 
 
+def main():
+    input_file = "../results/dataset1/simulations/train/simulation_0/dataset/simulated_dataset.tsv"
+    output_file = "dataset1_subsampled.tsv"
+    subsample_data(input_file, output_file, 50)
+
+
+if __name__ == "__main__":
+    main()
