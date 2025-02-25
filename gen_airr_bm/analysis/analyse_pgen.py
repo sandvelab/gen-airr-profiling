a8df65477ecb4fdb7401c81704fe43777def19e6
Charlotte-Wurtzen <charlotte.wurtzen@gmail.com>
2025-02-25 16:08:35 +0100

    feat: compute pgen files for generated sequences and save as helper data

--- gen_airr_bm/analysis/analyse_pgen.py
+++ gen_airr_bm/analysis/analyse_pgen.py
@@ -7,27 +7,37 @@ from gen_airr_bm.core.analysis_config import AnalysisConfig
 from gen_airr_bm.utils.olga_utils import compute_pgen
 
 
-def run_pgen_analysis(analysis_config: AnalysisConfig, output_dir: str):
-    train_sequences_dir = f"{output_dir}/train_sequences/{analysis_config.model_names}"
-    model_sequences_dir = f"{output_dir}/generated_sequences/{analysis_config.model_names}"
+def run_pgen_analysis(analysis_config: AnalysisConfig):
+    train_sequences_dir = f"{analysis_config.root_output_dir}/train_sequences/{analysis_config.model_names}"
+    models_dir = f"{analysis_config.root_output_dir}/generated_sequences"
 
-    olga_inputs = get_datasets(model_sequences_dir, output_dir, analysis_config.model_names, )
+    output_path_helper_data = os.path.join(analysis_config.root_output_dir, "helper_data")
+    os.makedirs(output_path_helper_data, exist_ok=True)
+
+    olga_inputs = get_datasets(models_dir, output_path_helper_data, analysis_config.model_names,
+                               analysis_config.default_model_name)
+
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(compute_pgen, seq_file, pgen_file, model) for seq_file, pgen_file, model in olga_inputs]
-
-    # train_pgen = get_pgens_all(train_sequences_dir, output_dir, analysis_config.model_name)
-    # model_pgen = get_pgens_all(model_sequences_dir, output_dir, analysis_config.model_name)
+        executor.map(lambda args: compute_pgen(*args), olga_inputs)
 
 
-def get_datasets(dataset_dir, output_dir, model_name, default_model_name):
-    datasets = os.listdir(dataset_dir)
+def get_datasets(models_dir, output_path_helper_data, model_names, default_model_name):
+
     olga_inputs = []
-    for dataset in datasets:
-        sequences_file_path = f"{dataset_dir}/{dataset}"
-        pgens_file_path = f"{output_dir}/{model_name}/{dataset.strip('.')[0]}_pgen.tsv"
-        olga_inputs.append([sequences_file_path, pgens_file_path, default_model_name])
+    for model_name in model_names:
+        os.makedirs(f"{output_path_helper_data}/{model_name}", exist_ok=True)
+        datasets = os.listdir(f"{models_dir}/{model_name}")
 
+        for dataset in datasets:
+            sequences_file_path = f"{models_dir}/{model_name}/{dataset}"
+            sequences_df = pd.read_csv(sequences_file_path, sep='\t')[["junction", "junction_aa", "v_call", "j_call"]]
+            sequence_file_path_olga = f"{output_path_helper_data}/{model_name}/{dataset.split('.')[0]}_olga_sequences.tsv"
+            sequences_df.to_csv(sequence_file_path_olga, sep='\t', index=False, header=False)
+
+            pgens_file_path = f"{output_path_helper_data}/{model_name}/{dataset.split('.')[0]}_pgen.tsv"
+            olga_inputs.append([sequence_file_path_olga, pgens_file_path, default_model_name])
     return olga_inputs
+
 
 def plot_pgen_distributions(pgens_all, model_name):
     pass
