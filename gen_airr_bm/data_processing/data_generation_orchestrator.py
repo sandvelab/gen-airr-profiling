3aa8780583f22ae5828133c1daa2c24b52b46bfa
marimam <marimam@uio.no>
2025-06-02 09:40:50 +0200

    feat: preprocess umi data

--- gen_airr_bm/data_processing/data_generation_orchestrator.py
+++ gen_airr_bm/data_processing/data_generation_orchestrator.py
@@ -1,13 +1,15 @@
 from gen_airr_bm.core.data_generation_config import DataGenerationConfig
 from gen_airr_bm.data_processing.data_generation_methods import (simulate_rare_and_frequent_olga_sequences,
-                                                                 preprocess_experimental_data)
+                                                                 preprocess_experimental_data,
+                                                                 preprocess_experimental_umi_data)
 
 
 class DataGenerationOrchestrator:
     """Orchestrates which simulation method to run based on the config."""
     DATA_GENERATION_METHODS = {
         "rare_and_frequent": simulate_rare_and_frequent_olga_sequences,
-        "experimental": preprocess_experimental_data
+        "experimental": preprocess_experimental_data,
+        "experimental_umi": preprocess_experimental_umi_data
     }
 
     def run_data_generation(self, config: DataGenerationConfig):
